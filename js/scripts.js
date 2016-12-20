1cb8d4a39adb8ecd2c433d1547babb6c226a0f8d
Nigel Burnett <nigelorion@gmail.com>
2016-12-20 15:24:33 -0800

    Add shop section and smooth scrolling back to top

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){
-  // Add smooth scrolling to all links
+
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 800) {
+        $('.home__top').fadeIn(600);
+      } else {
+        $('.home__top').fadeOut(600);
+      }
+    });
+
+  $('.home__top').click(function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({scrollTop: 0}, 500);
+  });
+
+  $("#header__click").on('click', function() {
+
+    $("#header__click--show").fadeIn(600);
+
+  })
+
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('#header__click--show').fadeOut(600);
+      }
+    });
+
   $("a").on('click', function(event) {
 
     if (this.hash !== "") {
 
       event.preventDefault();
-
 
       var hash = this.hash;
 
@@ -14,7 +39,7 @@ $(document).ready(function(){
       }, 800, function(){
 
         window.location.hash = hash;
-        
+
       });
     }
   });
